94e924aa7b9148087fe3dcbe3a83f8a5e82635ea
milkaotieno <milka.otieno@student.moringaschool.com>
2022-03-17 00:19:13 +0300

    keep departmentdao changes

--- src/main/java/models/dao/Sql2oUserDao.java
+++ src/main/java/models/dao/Sql2oUserDao.java
@@ -7,6 +7,7 @@ import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 import org.sql2o.Sql2oException;
 
+import java.sql.Connection;
 import java.util.List;
 
 public class Sql2oUserDao implements UserDao{
