a2d1787a49ef207e5b999d81f436c7b50b06151c
milkaotieno <milka.otieno@student.moringaschool.com>
2022-03-16 23:59:33 +0300

    keep departmentdao changes

--- src/main/java/models/dao/DepartmentDao.java
+++ src/main/java/models/dao/DepartmentDao.java
@@ -1,4 +1,20 @@
 package models.dao;
 
+import models.projos.Department;
+
+import java.util.List;
+
 public interface DepartmentDao {
+    //Create Department
+    void addDepartment(Department department);
+
+    //Read Department
+    List<Department> getAllDepartments();
+    Department getDepartmentById(int id);
+
+    //Update Department
+
+    //Delete Department
+    void deleteDepartmentById(int id);
+    void deleteAllDepartments();
 }
